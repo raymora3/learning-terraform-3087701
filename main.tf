8db2f8fc177a5d5512de60ed99d42da1a326c1a0
raymora3 <ray.mora@patientpoint.com>
2026-04-24 16:17:39 -0500

    added braces to lines 57 and 67

--- main.tf
+++ main.tf
@@ -54,6 +54,7 @@ resource "aws_security_group_rule" "blog_https_in" {
     cidr_blocks = ["0.0.0.0/0"]
 
     aws_security_group_id = aws_security_group.blog.id
+}
 
     resource "aws_security_group_rule" "blog_everything_out" {
     type       = "egress"
@@ -63,3 +64,4 @@ resource "aws_security_group_rule" "blog_https_in" {
     cidr_blocks = ["0.0.0.0/0"]
 
     aws_security_group_id = aws_security_group.blog.id
+    }
