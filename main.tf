76369d16cefa75afd495223b90ee58c232eda1eb
raymora3 <ray.mora@patientpoint.com>
2026-04-24 10:36:48 -0500

    changed instance type from t2-micro to c7i-flex.large

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+  instance_type = "c7i-flex.large"
 
   tags = {
     Name = "HelloWorld"
